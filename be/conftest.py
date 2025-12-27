a46acf8cbd361e75784dcdf0bc6a8dad0ecebcb4
Felix Hüfner <felix-huefner@gmx.de>
2025-12-27 16:31:48 +0100

    fix(be tests): atomate django migration

--- be/conftest.py
+++ be/conftest.py
@@ -7,14 +7,20 @@ from django.conf import settings
 
 
 @pytest.fixture(scope='session')
-def django_db_setup():
+def django_db_setup(django_db_blocker):
     """Override database setup to use SQLite in-memory for tests."""
+    from django.core.management import call_command
+    
     settings.DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': ':memory:',
         }
     }
+    
+    with django_db_blocker.unblock():
+        # Create tables using Django migrations
+        call_command('migrate', '--run-syncdb', verbosity=0)
 
 
 # pytest-django automatically handles Django setup
