643fc7c8870ddd1af780150ba35fbc9292318b1c
Felix Hüfner <felix-huefner@gmx.de>
2025-12-30 13:44:26 +0100

    feat(logout): add logaout function

--- fe/app/src/app/app.ts
+++ fe/app/src/app/app.ts
@@ -1,13 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { KeycloakService } from 'keycloak-angular';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet],
+    RouterOutlet,
+    MatIconModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   public readonly title = signal('app');
+  private keycloakService = inject(KeycloakService);
+
+  logout() {
+    this.keycloakService.logout(window.location.origin);
+  }
 }
